a5d7d52ccaa9e2dd8b3a2775e9c687d15d38cb63
ZaneleNkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-06-18 09:07:11 +0200

    ionic page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MessagePage } from '../pages/message/message';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MessagePage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MessagePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { MessagePage } from '../message/message';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from'@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  formgroup: FormGroup;
+  name: AbstractControl;
+  surname: AbstractControl;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public formbuilder: FormBuilder) {
+
+      this.formgroup = formbuilder.group({
+        name:['', Validators.required],
+        surname:['', Validators.required]
+      });
+
+      this.name = this.formgroup.controls['name'];
+      this.surname = this.formgroup.controls['surname'];
 
   }
+   onSubmit(data){
+  this.navCtrl.push(MessagePage, data);
+  
+  }
+
 
 }
